3c69fb4a86be04c5300d2b5e45e693d2e96eb2c3
Your Name <you@example.com>
2023-11-21 11:16:56 +0900

    gdg8

---
+++ checkpoints/gdg8/postnet/codes/20231121013553/run.py
@@ -0,0 +1,25 @@
+import os
+
+os.environ["OMP_NUM_THREADS"] = "1"
+
+from utils.commons.hparams import hparams, set_hparams
+import importlib
+
+
+def run_task():
+    assert hparams['task_cls'] != ''
+    pkg = ".".join(hparams["task_cls"].split(".")[:-1])
+    cls_name = hparams["task_cls"].split(".")[-1]
+    task_cls = getattr(importlib.import_module(pkg), cls_name)
+    # print(type(task_cls))
+    # print(f"Class reference: {task_cls}") # 打印类引用
+    # print(f"Class name: {task_cls.__name__}") # 打印类名
+    # return
+    task_cls.start()
+
+
+if __name__ == '__main__':
+    set_hparams()
+    print("sdz param final")
+    # print(hparams['load_db_to_memory'])
+    run_task()
